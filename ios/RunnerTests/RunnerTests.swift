b50b96890d4c8b862bc124a0e5e577a59aa9e811
Niladri Das <bniladridas@users.noreply.github.com>
2026-02-24 00:25:46 +0530

    fix: revert runner tests to origin/main

--- ios/RunnerTests/RunnerTests.swift
+++ ios/RunnerTests/RunnerTests.swift
@@ -6,13 +6,13 @@ import XCTest
 class RunnerTests: XCTestCase {
 
   func testAppDelegateWindowExists() {
-    let appDelegate = try XCTUnwrap(UIApplication.shared.delegate as? AppDelegate, "App delegate should exist")
-    let _ = try XCTUnwrap(appDelegate.window, "App delegate should have a window")
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    XCTAssertNotNil(appDelegate.window, "App delegate should have a window")
   }
 
   func testAppDelegateWindowHasRootViewController() {
-    let appDelegate = try XCTUnwrap(UIApplication.shared.delegate as? AppDelegate, "App delegate should exist")
-    let _ = try XCTUnwrap(appDelegate.window?.rootViewController, "Window should have a root view controller")
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    XCTAssertNotNil(appDelegate.window?.rootViewController, "Window should have a root view controller")
   }
 
 }
